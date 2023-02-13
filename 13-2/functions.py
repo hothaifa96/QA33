270b5ec9b8ff0d0ace77038aecfea6d1394822e4
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-02-13 20:56:47 +0200

    message

---
+++ 13-2/functions.py
@@ -0,0 +1,9 @@
+# define a function to print yaouza 4 times
+
+# define a function to print a word of the user choice using input
+
+# a golden name is a name with 6 letters and includes i in it
+# define a function to print if the given name is golden or not
+
+# define a function to receive a text as argument and write add it to a new
+# text file named aa.txt and call it log
